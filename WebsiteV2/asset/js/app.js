6f191c1ab062fcd9c6ca9927ced606cd2960d2a4
125322879 <hasanarbabi90@gmail.com>
2021-06-03 14:18:15 +0430

    first commit

--- WebsiteV2/asset/js/app.js
+++ WebsiteV2/asset/js/app.js
@@ -9,38 +9,73 @@ $.ajax({
         console.log(res);
         for (i = 0; i < res.length; i++) {
 
-            let item ='';
-            item+='<div class="item rect p-2 my-2">';
-            item+='<div class="row">';
-            item+='<div class="col d-flex align-items-center">';
-            item+='<div class="mx-1 dot1"></div>';
-            item+='<div class="mx-2 fs3">' + res[i].source_location._shahr.shahrname + '</div>';
-            item+='<div class="fs4">' + res[i].source_location._ostan.provincename + '</div>';
-            item+='</div>';
-            item+='</div>';
-            item+='<div class="row">';
-            item+='<div class="col d-flex align-items-center py-1">';
-            item+='<div class="mx-1 dot2"></div>';
-            item+='<div class="mx-2 fs3">' + res[i].destination_location._shahr.shahrname + '</div>';
-            item+='<div class="fs4">' + res[i].destination_location._ostan.provincename + '</div>';
-            item+='</div>';
-            item+='</div>';
-            item+='<div class="row">';
-            item+='<div class="col">';
-            item+='<div class="fs5 rect2 d-flex align-items-center justify-content-center py-1">' + res[i].vehicle_requested_type.t_name + '</div>';
-            item+='</div>';
-            item+='</div>';
-            item+='<div class="row">';
-            item+='<div class="col">';
-            item+='<div class="fs5 d-flex align-items-center p-1">' + res[i].load_type.tname + '</div>';
-            item+='</div>';
-            item+='</div>';
-            item+='<div class="row">';
-            item+='<div class="col">';
-            item+='<div class="fs6 rect3 d-flex align-items-center justify-content-center py-1">' + res[i].status_persian + '</div>';
-            item+='</div>';
-            item+='</div>';
-            item+='</div>';
+            switch (res[i].status) {
+                case 0:
+                    status = "recti"
+                    break;
+                case 1:
+                    status = "rectd"
+                    break;
+                case 2:
+                    status = "rectd"
+                    break;
+                case 3:
+                    status = "rectd"
+                    break;
+                case 4:
+                    status = "rects"
+                    break;
+                case 5:
+                    status = "rectf"
+                    break;
+                case -1:
+                    status = "rects"
+                    break;
+                case -2:
+                    status = "rectf"
+                    break;
+                case -3:
+                    status = "recti"
+                    break;
+                case 10:
+                    status = "rectf"
+                    break;
+                default:
+                    status = "rectf"
+            };
+
+            let item = '';
+            item += '<div class="item rect p-2 my-2">';
+            item += '<div class="row">';
+            item += '<div class="col d-flex align-items-center">';
+            item += '<div class="mx-1 dot1"></div>';
+            item += `<div class="mx-2 fs3">${res[i].source_location._shahr.shahrname}</div>`;
+            item += `<div class="fs4">${res[i].source_location._ostan.provincename}</div>`;
+            item += '</div>';
+            item += '</div>';
+            item += '<div class="row">';
+            item += '<div class="col d-flex align-items-center py-1">';
+            item += '<div class="mx-1 dot2"></div>';
+            item += `<div class="mx-2 fs3">${res[i].destination_location._shahr.shahrname}</div>`;
+            item += `<div class="fs4">${res[i].destination_location._ostan.provincename}</div>`;
+            item += '</div>';
+            item += '</div>';
+            item += '<div class="row">';
+            item += '<div class="col">';
+            item += `<div class="fs5 rect2 d-flex align-items-center justify-content-center py-1">${res[i].vehicle_requested_type.t_name}</div>`;
+            item += '</div>';
+            item += '</div>';
+            item += '<div class="row">';
+            item += '<div class="col">';
+            item += `<div class="fs5 d-flex align-items-center p-1">${res[i].load_type.tname}</div>`;
+            item += '</div>';
+            item += '</div>';
+            item += '<div class="row">';
+            item += '<div class="col">';
+            item += `<div class="fs6 ${status} d-flex align-items-center justify-content-center py-1">${res[i].status_persian}</div>`;
+            item += '</div>';
+            item += '</div>';
+            item += '</div>';
 
             $(".owl-carousel").append(item);
 
@@ -108,3 +143,18 @@ $(document).ready(function () {
 });
 
 AOS.init();
+
+String.prototype.toPersianDigits = function () {
+    var id = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    return this.replace(/[0-9]/g, function (w) {
+        return id[+w]
+    });
+}
+
+function toFarsiNumber(n) {
+    const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+    return n
+        .toString()
+        .replace(/\d/g, x => farsiDigits[x]);
+}
